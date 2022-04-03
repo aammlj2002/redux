9c6a6b3d944da88b2e92f38d863290d869d15a8d
Leo <aammlj2002@gmail.com>
2022-04-03 14:01:32 +0630

    make a api request form home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,22 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 function Home() {
+    const apiKey = "dbfc6703";
+    const searchKey = "Harry";
+    useEffect(() => {
+        const fetchMovie = async () => {
+            try {
+                const res = await axios.get(
+                    `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchKey}"`
+                );
+                console.log(res);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchMovie();
+    }, []);
     return <div>Home</div>;
 }
 
