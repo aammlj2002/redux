8691945601a3cdefdeb08699d17c1206088d5931
Leo <aammlj2002@gmail.com>
2022-04-03 15:28:43 +0630

    setup redux

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,33 @@
 import axios from "axios";
 import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
+import MovieListing from "../MovieListing/MovieListing";
 
 function Home() {
-    const apiKey = "dbfc6703";
+    const dispatch = useDispatch();
+    const apikey = "dbfc6703";
     const searchKey = "Harry";
     useEffect(() => {
         const fetchMovie = async () => {
             try {
                 const res = await axios.get(
-                    `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchKey}"`
+                    `http://www.omdbapi.com/?apikey=${apikey}&s=${searchKey}"`
                 );
                 console.log(res);
+                dispatch(addMovies(res.data));
             } catch (error) {
                 console.log(error);
             }
         };
+
         fetchMovie();
     }, []);
-    return <div>Home</div>;
+    return (
+        <div>
+            <MovieListing />
+        </div>
+    );
 }
 
 export default Home;
---
+++ src/features/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import MovieReducer from "./movies/movieSlice";
+export const store = configureStore({
+    reducer: {
+        movies: MovieReducer,
+    },
+});
