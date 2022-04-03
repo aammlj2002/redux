3b9e432346523464bdc5cf41f2d3bb73b159770f
Leo <aammlj2002@gmail.com>
2022-04-03 16:12:44 +0630

    add footer design

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Footer() {
-    return <div>Footer</div>;
+    return (
+        <>
+            <footer class="bg-white dark:bg-gray-800 w-full py-8">
+                <div class="max-w-screen-xl mx-auto px-4">
+                    <div className="text-white text-center">Copyright 2022</div>
+                </div>
+            </footer>
+        </>
+    );
 }
 
 export default Footer;
